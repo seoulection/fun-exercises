78a784ec14f2357b98a0191ef877067f7f24b9ec
akemim12 <akemimurayama12@gmail.com>
2021-10-03 17:40:33 -0700

    create hello world in ruby

--- hello_world/spec/hello_world_spec.rb
+++ hello_world/spec/hello_world_spec.rb
@@ -2,6 +2,6 @@ require "hello_world"
 
 RSpec.describe HelloWorld do
   it "returns 'Hello World'" do
-    expect(HelloWorld.hello_world).to eq "WHAT SHOULD THIS BE?"
+    expect(HelloWorld.hello_world).to eq "Hello World"
   end
 end
